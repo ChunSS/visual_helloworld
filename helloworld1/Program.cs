00c8cf4cb24769ee48cd6b0a6126b7a729560f2f
KangSukChun <ksc9910@naver.com>
2019-11-07 10:03:49 +0900

    master : 2nd Hello World

--- helloworld1/Program.cs
+++ helloworld1/Program.cs
@@ -12,6 +12,7 @@ namespace helloworld1
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("hotfix : Hello World");
+            Console.WriteLine("master : 2nd Hello World");
         }
     }
 }
