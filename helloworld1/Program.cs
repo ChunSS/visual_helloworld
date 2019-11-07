9478493ca2e81067d6c446efa0cf7759399496a4
KangSukChun <ksc9910@naver.com>
2019-11-07 10:03:04 +0900

    hotfix WriteLine Commit

--- helloworld1/Program.cs
+++ helloworld1/Program.cs
@@ -11,6 +11,7 @@ namespace helloworld1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("hotfix : Hello World");
         }
     }
 }
